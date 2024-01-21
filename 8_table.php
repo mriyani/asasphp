53e67c7248d94e4e29607e4fa199a8c54edb7dcf
mriyani <ryaneq@gmail.com>
2024-01-22 01:39:21 +0800

    Update table row's color

--- 8_table.php
+++ 8_table.php
@@ -4,6 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Create Table</title>
+    <style>
+        .warna-baris {
+            background-color: salmon;
+        }
+    </style>
 </head>
 <body>
 
@@ -20,16 +25,18 @@
         </tr>
     </table>
     <br>
-    <table border="1" cellpadding="10" cellspacing="0" bgcolor="salmon">
-        <?php 
-            for ($i=1; $i<=3; $i++) {
-                echo "<tr>";
-                for ($j=1; $j<=5; $j++) {
-                    echo "<td>$i,$j</td>";
-                }
-                echo "</tr>";
-            }
-        ?>
+    <table border="1" cellpadding="10" cellspacing="0">
+        <?php for ($i=1; $i<=7; $i++) { ?>
+            <?php if ($i % 2 == 1) { ?>
+                <tr class="warna-baris">
+            <?php } else { ?>
+                <tr>
+            <?php } ?>
+                <?php for ($j=1; $j<=7; $j++) { ?>
+                    <td><?= "$i,$j"; ?></td>
+                <?php } ?>
+                </tr>
+        <?php } ?>
     </table>
 </body>
 </html>
