0f59c835f425d96596faa65364aa8e842862967e
mriyani <ryaneq@gmail.com>
2024-01-29 00:52:23 +0800

    Add date file

---
+++ 9_date.php
@@ -0,0 +1,10 @@
+<?php
+
+// Date
+echo "Today is " . date("l, d-M-Y") . "<br><br>";
+
+// Time
+echo time() . " => Kiraan saat bermula dari 1 Januari 1970<br><br>";
+
+// Kira umur
+echo date('Y') - 1986;
