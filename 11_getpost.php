ac94f0ec9a20f554be27eba89a12704cf47b4184
mriyani <ryaneq@gmail.com>
2024-02-04 22:46:11 +0800

    Update getpost and add profile image folder

--- 11_getpost.php
+++ 11_getpost.php
@@ -6,13 +6,59 @@
 // var_dump($_SERVER);
 
 // $_GET
-var_dump($_GET);
+// var_dump($_GET);
 
-
-
-//$_POST
-var_dump($_POST);
-
-
+$pelajar = [
+    [
+        "nama" => "Muhammad Riyani",
+        "ndp" => "0123654",
+        "email" => "email@yehaa.com",
+        "kursus" => "Rangkaian Komputer",
+        "gambar" => "man.png"
+    ],  
+    [
+        "nama" => "Nur Amni",
+        "ndp" => "654123",
+        "email" => "email@yuhoo.com",
+        "kursus" => "Sistem Komputer",
+        "gambar" => "woman.png"
+    ]
+];
 
 ?>
+
+<style>
+    img {
+        width: 50px;
+        height: auto,
+    };
+</style>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GET</title>
+</head>
+<body>
+<h1>Daftar Pelajar</h1>
+<?php foreach( $pelajar as $pljr ) : ?>
+    <ul>
+        <li><img src="img/<?= $pljr["gambar"]; ?>" alt="Profile"></li>
+        <li><?= $pljr["nama"]; ?></li>
+        <li><?= $pljr["ndp"]; ?></li>
+        <li><?= $pljr["email"]; ?></li>
+        <li><?= $pljr["kursus"]; ?></li>
+    </ul>
+<?php endforeach ?>
+</body>
+</html>
+
+
+
+
+
+<?php
+//$_POST
+//var_dump($_POST);
