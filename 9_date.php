162414c5ae45d85af4c90f258257c0e6b4e6ca47
mriyani <ryaneq@gmail.com>
2024-02-05 17:13:40 +0800

    Update age formula on date.php

--- 9_date.php
+++ 9_date.php
@@ -6,5 +6,6 @@ echo "Today is " . date("l, d-M-Y") . "<br><br>";
 // Time
 echo time() . " => Kiraan saat bermula dari 1 Januari 1970<br><br>";
 
+
 // Kira umur
-echo date('Y') - 1986;
+echo "Umur saya adalah " . date('Y') - 1999 . " tahun";
