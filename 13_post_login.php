e6d7ee3af78142537bf3f9cb8768f71a2016ed30
mriyani <ryaneq@gmail.com>
2024-02-09 17:49:12 +0800

    Update login authentication using array data

--- 13_post_login.php
+++ 13_post_login.php
@@ -1,18 +1,84 @@
 <?php
+
+$pelajar = [
+    [
+        "username" => "riyani",
+        "nama" => "Muhammad Riyani",
+        "ndp" => "0123654",
+        "email" => "email@yehaa.com",
+        "kursus" => "Rangkaian Komputer",
+        "gambar" => "man.png",
+        "password" => "123"
+    ],  
+    [
+        "username" => "amni",
+        "nama" => "Nur Amni",
+        "ndp" => "654123",
+        "email" => "email@yuhoo.com",
+        "kursus" => "Sistem Komputer",
+        "gambar" => "girl.png",
+        "password" => "123"
+    ],
+    [
+        "username" => "aboy",
+        "nama" => "Aboy Hensome",
+        "ndp" => "456987123",
+        "email" => "email@boboy.com",
+        "kursus" => "Pomen",
+        "gambar" => "boy.png",
+        "password" => "123"
+    ],
+    [
+        "username" => "cahaya",
+        "nama" => "Cahaya Purnama",
+        "ndp" => "741852369",
+        "email" => "nur@purnama.com",
+        "kursus" => "Baking",
+        "gambar" => "woman.png",
+        "password" => "123"
+    ],  
+];
+
+?>
+
+<?php
 // Semak butang Login ditekan
 if (isset($_POST['submit'])) {
-    // Semak username dan password
-    if ($_POST['username'] == 'Admin' && $_POST['password'] == 123) {
-        // Jika BETUL, redirect ke User Profil
-        header('Location: 14_post_detail.php?name=' . $_POST['username']);
-        exit();
+
+    // Semak username dan password diberikan oleh user
+    if (isset($_POST['username']) && isset($_POST['password'])) {
+        global $pelajar;
+
+        // Simpan data ndp dan password ke dalam variable
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+
+        // Loop ke dalam array $pelajar untuk cek username dan password
+        foreach ($pelajar as $pelajar1) {
+            if ($pelajar1['username'] == $username && $pelajar1['password'] == $password) {
+
+                // Jika BETUL, redirect ke User Profil
+                $nama = $pelajar1['nama'];
+                $gambar = $pelajar1['gambar'];
+                $ndp = $pelajar1['ndp'];
+                $email = $pelajar1['email'];
+                $kursus = $pelajar1['kursus'];
+                header('Location: 14_post_detail.php?name=' . $nama . '&img='. $gambar . '&ndp=' . $ndp . '&email=' . $email . '&kursus=' . $kursus);
+                exit();
+            }
+        }
+        // Jika SALAH, paparkan error message
+        $error = true;
+
     } else {
         // Jika SALAH, paparkan error message
         $error = true;
     }
 
 }
+
 ?>
+
 
 <!DOCTYPE html>
 <html lang="en">
@@ -29,9 +95,9 @@ if (isset($_POST['submit'])) {
 
     <form action="" method="POST">
         <label for="username">Username:</label><br>
-        <input type="text" name="username" id="username" ><br>
+        <input type="text" name="username" id="username" required><br>
         <label for="password">Password:</label><br>
-        <input type="password" name="password" id="password" ><br><br>
+        <input type="password" name="password" id="password" required><br><br>
         <button type="submit" name="submit">Login</button>
     </form>
 
--- 14_post_detail.php
+++ 14_post_detail.php
@@ -6,8 +6,15 @@
     <title>User Profile</title>
 </head>
 <body>
-    <?php var_dump($_GET); ?>
+    <!-- <?php var_dump($_GET); ?> -->
+    <h1>User Profil</h1>
+    <img width="80px" height="auto" src=img/<?= $_GET['img']; ?> >
     <h1>Selamat Datang, <?= $_GET['name']; ?></h1>
+    <ul>
+        <li>NDP: <?= $_GET['ndp']; ?></li>
+        <li>Email: <?= $_GET['email']; ?></li>
+        <li>Kursus: <?= $_GET['kursus']; ?></li>
+    </ul>
 
     <a href="13_post_login.php">Logout</a>
 </body>
