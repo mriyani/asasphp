7f6246f40c0c2ad137fcf422889800d242995f03
mriyani <ryaneq@gmail.com>
2024-03-14 16:27:43 +0800

    Add COOKIE feature

--- daftarpelajar/6_login.php
+++ daftarpelajar/6_login.php
@@ -1,12 +1,30 @@
 <?php
 session_start();
 
+require_once '1_functions.php';
+
+// Cek cookie
+if (isset($_COOKIE['id']) && isset($_COOKIE['key'])) {
+    $id = $_COOKIE['id'];
+    $key = $_COOKIE['key'];
+
+    // Ambil username berdasarkan id
+    $result = mysqli_query($condb, "SELECT username FROM login WHERE id = '$id'");
+    $row = mysqli_fetch_assoc($result);
+    $username = $row['username'];
+
+    if ($key === hash('sha512', $username)) {
+        $_SESSION['login'] = true;
+
+    }
+
+}
+
 if (isset($_SESSION['login'])) {
     header('Location: index.php');
     exit;
 }
 
-require_once '1_functions.php';
 
 if (isset($_POST['login'])) {
     $username = $_POST['username'];
@@ -22,6 +40,14 @@ if (isset($_POST['login'])) {
         if (password_verify($password, $row['password'])) {
             // Set session
             $_SESSION['login'] = true;
+
+            // Cek remember me
+            if (isset($_POST['remember'])) {
+                // Buat cookie
+                // setcookie('login', 'true', time() + 60);
+                setcookie('id', $row['id'], time() + 60);
+                setcookie('key', hash('sha512', $row['username']), time() + 60);
+            }
 
             header("Location: index.php");
             exit;
@@ -56,6 +82,10 @@ if (isset($_POST['login'])) {
             </li>
             <li><label for="password">Password: </label><br>
                 <input type="password" name="password" id="password"><br><br>
+            </li>
+            <li>
+                <input type="checkbox" name="remember" id=" remember">
+                <label for="remember">Remember me</label><br><br>
             </li>
             <button type="submit" name="login">Login</button>
         </ul>
--- daftarpelajar/7_logout.php
+++ daftarpelajar/7_logout.php
@@ -1,8 +1,15 @@
 <?php
 session_start();
+
+// Clear and destroy session
 $_SESSION = [];
 session_unset();
 session_destroy();
 
+// Clear cookie
+setcookie('id', '', time() - 3600);
+setcookie('key', '', time() - 3600);
+
+// Redirect to login page
 header('Location: 6_login.php');
 exit;
